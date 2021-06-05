e7e37ffa4ce956b075bc52c4015e4f38fecd35e9
Leandro Henrick <leandrohenrick96@hotmail.com>
2021-06-05 11:28:55 -0300

    Adicionando outras questões

--- src/App.java
+++ src/App.java
@@ -1,13 +1,27 @@
 public class App {
     public static void main(String[] args) throws Exception {
                 
-        // 1) Faça um algoritmo recursivo que implemente a sequência fibonacci;
+        /* 1) Faça um algoritmo recursivo que implemente a sequência fibonacci; */
        
         for (int i = 0; i < 10; i++) {
             System.out.print(" " + Fibonacci.fibonacci(i));
         }
 
+        /* 2) Para os algoritmos de ordenação faça alterações nos algoritmos 
+              insertion sort, selection sort e bubble sort, para que o programa 
+              contempla o envio de duas variáveis indicando as posições inicial e 
+              final do vetor que desejo ordenar Deve realizar as devidas alteração 
+              na implementação e seus devidos testes;*/
+
+        /* 3) Para o algoritmo quicksort desenvolvido durante a aula, faça as 
+        seguintes alterações:
         
+        a. Implemente o método particiona e coloque no seu escopo toda a lógica referente ao particionamento;
+
+        b. Pesquisa e altere o código do quicksort para que a escolha no pivô seja através do cálculo da mediana;*/
          
+        /* 4) Para o algoritmo mergesort implemente a alterações que possibilite o 
+        algoritmo ordenar deste número inteiros até objetos; (dica pesquise sobre 
+        a interface Comparable); */
     }
 }
